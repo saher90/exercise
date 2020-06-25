4b74e84d4ff32dd7be8a2eaf96b8fbfee5896437
Abziz <Abziz89@gmail.com>
2020-06-25 11:16:31 +0300

    use port from env variables

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth');
 const db = require('./data/database');
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 const { authorized, parseUser, anonymouse } = require('./middlewares/auth');
@@ -25,4 +26,4 @@ app.get('/404', (req, res) => res.status(404).sendFile(path.resolve(__dirname, '
 app.get('/protected', authorized, (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'protected.html')));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'home.html')));
 app.use('/', (req, res) => res.redirect('/404'));
-app.listen(5000);
+app.listen(PORT);
